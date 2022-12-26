ce1cde7c2c45d1a2f9f9f3562d4c01bde3d3035a
Mow1o <tuomokemi@gmail.com>
2022-12-26 13:26:59 +0200

    1

--- c++/input_to_file.cpp
+++ c++/input_to_file.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 int main(void) {
 ofstream MyFile("input.txt");
-char text[100];
-
-
-cin.getline(text, 100);
+char text[11];
+scanf("%10[0-9a-zA-Z ]", text);
 
 
 MyFile << text;
+
 MyFile.close();
 
 }
