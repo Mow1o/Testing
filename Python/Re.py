e9148b96f43be39da0e1be5da8598477936ef33f
Mow1o <tuomokemi@gmail.com>
2023-05-02 11:52:31 +0300

    changes

--- Python/Re.py
+++ Python/Re.py
@@ -25,14 +25,32 @@ print (len(pattern))
 
 
 
-# 4
+# 4 string variable
 
 raw = "economic"
 pattern = re.findall(raw, text)
 print(pattern)
 
-# 5
+# 5 Simple without regex
 
 raw = "economic"
-pattern = re.findall(raw, text)
-print(pattern)
+if raw in text:
+    print("yes")
+
+
+# 6 Simple list and regex.findall
+
+raw = ["economic", "economical", "economy"]
+for i in raw:
+    pattern = re.findall(i, text)
+    print(len(pattern))
+
+# 7 Simple list and regex match
+
+raw = ["economic", "economical", "economy"]
+for i in raw:
+    pattern = re.match(i, text)
+    print(pattern)
+
+
+
