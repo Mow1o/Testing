6698b9ac9e66b176d7c54bce5c65e395a636b07f
Mow1o <tuomokemi@gmail.com>
2023-05-02 12:08:25 +0300

    changes

--- Python/Re.py
+++ Python/Re.py
@@ -1,7 +1,7 @@
 import re
 
 
-# 1 find phone number in the text
+# 1 find phone number string in the text
 text = 'I have called the service desk 100 times and nobody replies to me. I need a conversation ASAP!! My number is 111-1234567!'
 result = re.findall('111-1234567', text)
 print(result)
@@ -10,7 +10,7 @@ print(result)
 
 #2 find all phone numbers in the text
 text = '111-1234567! That is my number! The other one is 7654321!'
-result = re.findall(r"^\d{3}-\d{7}|^\d{7}", text)
+result = re.findall(r"^\d{3}\d{7}|^\d{7}", text)
 print (result)
 
 
@@ -51,6 +51,9 @@ raw = ["economic", "economical", "economy"]
 for i in raw:
     pattern = re.match(i, text)
     print(pattern)
+
+# 8
+
 
 
 
